2b2aed7e9d2e433aa70c38e85730d6ff14f86df0
Deepak Kumar <1172dipu@gmail.com>
2024-08-19 00:11:50 +0530

    setup cloudinary

--- src/models/video.models.js
+++ src/models/video.models.js
@@ -1,54 +1,50 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
-    {
-        videoFile: {
-            type: String,  //cloudnary url
-            required: true
-        },
-
-        
-        thumbnail: {
-            type: String,
-            required: true
-        },
-
-        title: {
-            type: String,  
-            required: true
-        },
-
-        description: {
-            type: String,  
-            required: true
-        },
-
-        duration: {
-            type: Number,  //cloudnary url
-            required: true
-        },
-
-        views: {
-            type: Number,  
-            default: 0
-        },
-
-        isPublished: {
-            type: Boolean,  
-            default: true,
-        },
-        videoOwner:{
-            type: Schema.Types.ObjectId,
-            ref: "User"
-        }
-
-
+  {
+    videoFile: {
+      type: String, //cloudnary url
+      required: true,
     },
-    {timestamps: true}
-)
 
+    thumbnail: {
+      type: String,
+      required: true,
+    },
 
-videoSchema.plugin(mongooseAggregatePaginate)
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    duration: {
+      type: Number, //cloudnary url
+      required: true,
+    },
+
+    views: {
+      type: Number,
+      default: 0,
+    },
+
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    videoOwner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
 
 export const Video = mongoose.model("Video", videoSchema);

b20981eb220d54fcea4fc2b57b4c94f839c96ff7
Deepak Kumar <1172dipu@gmail.com>
2025-04-27 01:26:00 +0530

    DB connection

--- src/db/index.js
+++ src/db/index.js
@@ -16,3 +16,25 @@ const connectDB = async () => {
 };
 
 export default connectDB;
+
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "../constants.js";
+// import express from "express"
+// const app = express()
+
+// ( async () => {
+//   try{
+//     await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//     app.on("error", (error) => {
+//       console.log("error:" , error);
+//       throw error
+//     })
+//     app.listen(process.env.PORT, ()=> {
+//       console.log(`App is listening on pport ${process.env.PORT}`)
+//     })
+//   } catch (error){
+//     console.error("ERROR ", error)
+//     throw err
+//   }
+// })()
